b24f12557cf8619ebce7d976634dbd0dad56add0
VINITM-M <vinithsinthamani@gmail.com>
2024-03-31 09:02:30 +0530

    feat:execute

--- Sortingalphabetsbasedonfrequency.c
+++ Sortingalphabetsbasedonfrequency.c
@@ -1,1 +1,30 @@
 
+
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    char s[] = "abcdabcd";
+    int len =strlen(s);
+    int a[10001] = {0};
+    for(int i=0;i<len;i++){
+        a[s[i]]++;
+    }
+    int mx =1;
+    int index = 0;
+    while(mx!=0){
+        mx=0;
+        for(int i=63;i<123;i++){
+            if(mx<a[i]){
+                mx = a[i];
+                index = i;
+            }
+        }
+        a[index] =0;
+        for(int i=0;i<mx;i++){
+            printf("%c",index);
+        }
+    }
+    
+    }
